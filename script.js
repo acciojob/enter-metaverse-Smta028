43981c1d98bc4e1120b952b5b473e9e89e607ea9
Samta <108336676+Smta028@users.noreply.github.com>
2023-04-14 00:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p=document.querySelector("#status");
+let mybtn=document.querySelector("#enterBtn");
+
+mybtn.addEventListener(click,function()){
+	p.innerHTML=`<h1>Enter the Metaverse</h1>`
+	
+}
